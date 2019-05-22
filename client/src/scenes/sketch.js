468b530360746149ce92e270fd59ca086b80805b
anon <sarah.anne.friend@gmail.com>
2019-05-22 10:13:22 -0400

    Max node is coloured

--- client/src/scenes/sketch.js
+++ client/src/scenes/sketch.js
@@ -1,4 +1,26 @@
+const countConnections= (nodes, edges) => {
+  return nodes.map(n => {
+    n.count = edges.reduce((count, e) => {
+      if (e.toId === n.id || e.fromId === n.id) {
+        count += 1
+      }
+      return count
+    }, 0)
+    return n
+  })
+}
 
+const maxConnections= (nodes) => {
+  let max = nodes.reduce((max, n) => {
+    if (n.count > max) return n.count;
+    return max;
+  }, 0)
+  return nodes.map(n => {
+    if (n.count === max) { n.max = true }
+    else { n.max = false }
+    return n;
+  })
+}
 
 
 export default function (p) {
@@ -35,12 +57,15 @@ export default function (p) {
   p.draw = () => {
   	if (p.props.links.length !== edges.length) {
   	  console.log(p.props.links)
-      nodes = p.props.nodes.map((n) => {
-        p.fill(255)
+      nodes = countConnections(p.props.nodes, p.props.links)
+      nodes = maxConnections(nodes)
+      nodes = nodes.map((n) => {
+        if (n.max) { p.fill(40, 255, 40) }
+        else { p.fill(255) }
         p.stroke(255)
         p.circle(n.x, n.y, 20)
         p.fill(0)
-        p.text(n.public, n.x, n.y);
+        p.text(`${n.public} ${n.count}`, n.x, n.y);
         return n
       })
       edges = p.props.links.map(e => {
@@ -52,12 +77,15 @@ export default function (p) {
 
     if (dragging) {
       p.background(200)
-      nodes = p.props.nodes.map((n) => {
-        p.fill(255)
+      nodes = countConnections(p.props.nodes, p.props.links)
+      nodes = maxConnections(nodes)
+      nodes = nodes.map((n) => {
+        if (n.max) { p.fill(40, 255, 40) }
+        else { p.fill(255) }
         p.stroke(255)
         p.circle(n.x, n.y, 20)
         p.fill(0)
-        p.text(n.public, n.x, n.y);
+        p.text(`${n.public} ${n.count}`, n.x, n.y);
         return n
       })
       edges = p.props.links.map(e => {
